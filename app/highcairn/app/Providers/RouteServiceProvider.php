dfcc4405a3b1f963879e473c866314b3b2168b9a
KTaskn <korai.kinjo@gmail.com>
2020-11-28 19:03:24 +0900

    add mysql

--- app/highcairn/app/Providers/RouteServiceProvider.php
+++ app/highcairn/app/Providers/RouteServiceProvider.php
@@ -26,7 +26,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace_api = 'App\\Http\\Controllers\\Api';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -40,7 +41,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::prefix('api')
                 ->middleware('api')
-                ->namespace($this->namespace)
+                ->namespace($this->namespace_api)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
