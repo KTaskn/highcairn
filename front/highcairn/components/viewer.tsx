11d688b12b7f44c8520a37987d1039d853dc5265
kinjo-krs <kinjo-krs@corp.khi.co.jp>
2020-12-14 15:44:37 +0900

    Viewerコンポーネントを追加

---
+++ front/highcairn/components/viewer.tsx
@@ -0,0 +1,14 @@
+import * as MarkdownIt from 'markdown-it'
+import mk from 'markdown-it-katex'
+
+const md = new MarkdownIt()
+md.use(mk)
+
+const Viewer: React.FC<{markdown_text: string}> = ({markdown_text}) => {
+  let html: string = md.render(markdown_text)
+  return (
+    <div dangerouslySetInnerHTML={{__html: html}}></div>
+  )
+}
+
+export default Viewer
