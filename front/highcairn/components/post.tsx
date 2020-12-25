40fc5abe036f77e25c0b4735bb01a2dc36f8f8c5
kinjo-krs <kinjo-krs@corp.khi.co.jp>
2020-12-25 14:40:06 +0900

    タイトルにページへのリンクをついか

--- front/highcairn/components/post.tsx
+++ front/highcairn/components/post.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Post: React.FC<Props> = (props) => (
     <div>
-        <h1>{props.postmodel.title}</h1>
+        <h1><a href={`/post/${props.postmodel.id}`}>{props.postmodel.title}</a></h1>
         <Viewer content={props.postmodel.content}/>
     </div>
 )
