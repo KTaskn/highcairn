68246ede3b3077c3b9bfe3d3be4a1553ac63e95e
KTaskn <korai.kinjo@gmail.com>
2020-12-27 18:12:06 +0900

    ポストの日付周り追加

--- front/highcairn/components/post.tsx
+++ front/highcairn/components/post.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import dayjs from 'dayjs'
 import PostModel from '../models/post'
 import Viewer from '../components/viewer'
 import style from '../styles/components/Post.module.scss'
@@ -8,7 +9,8 @@ interface Props {
 }
 
 const Post: React.FC<Props> = (props) => (
-    <div>
+    <div className={style.post}>
+        <span>{dayjs(props.postmodel.created_at).locale('ja').format('YYYY-MM-DD')}</span>
         <h1 className={style.title}><a href={`/post/${props.postmodel.id}`}>{props.postmodel.title}</a></h1>
         <Viewer content={props.postmodel.content}/>
     </div>
--- front/highcairn/models/post.ts
+++ front/highcairn/models/post.ts
@@ -3,6 +3,8 @@ export default class Post {
     id: number
     title: string
     content: string
+    created_at: Date
+    updated_at: Date
 
     constructor(init?: Partial<Post>) {
         Object.assign(this, init)
