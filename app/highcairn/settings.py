b0fccabcc575b58750af3d68b2707ea7ac6bf95f
KTaskn <korai.kinjo@gmail.com>
2020-12-27 17:48:30 +0900

    datetimeを追加

--- app/highcairn/settings.py
+++ app/highcairn/settings.py
@@ -112,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-JP'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -137,5 +137,3 @@ REST_FRAMEWORK = {
        'django_filters.rest_framework.DjangoFilterBackend'
     ]
 }
-
-APPEND_SLASH = True
--- app/highcairnapp/models.py
+++ app/highcairnapp/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30, blank=False, null=False)
     content = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
