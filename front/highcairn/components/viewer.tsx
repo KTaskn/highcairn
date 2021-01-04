69c1d3541d24252d411283e3c3961d30f01ec10b
KTaskn <korai.kinjo@gmail.com>
2021-01-04 18:31:15 +0900

    highlightを追加

--- front/highcairn/components/viewer.tsx
+++ front/highcairn/components/viewer.tsx
@@ -1,10 +1,22 @@
 import * as MarkdownIt from 'markdown-it'
 import mk from 'markdown-it-katex'
+import hljs from 'highlight.js'
 import 'katex/dist/katex.min.css'
+import 'highlight.js/styles/hopscotch.css'
 
 import style from '../styles/components/Viewer.module.scss'
 
-const md = new MarkdownIt()
+const md = new MarkdownIt({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value
+      } catch (__) {}
+    }
+
+    return ''
+  }
+})
 md.use(mk)
 
 const Viewer: React.FC<{content: string}> = ({content}) => {
