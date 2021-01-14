ea8d1ebd37673fb2131beb905e75de1a6ce756b6
KTaskn <korai.kinjo@gmail.com>
2021-01-14 10:22:06 +0900

    imageモデルとuploadメソッドを追加

--- app/highcairnapp/models.py
+++ app/highcairnapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Post(models.Model):
@@ -7,3 +8,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     public = models.BooleanField(default=False)
+
+class Image(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content = models.BinaryField()
--- app/highcairnapp/serializers.py
+++ app/highcairnapp/serializers.py
@@ -1,5 +1,11 @@
+import base64
+from io import BytesIO
+from PIL import Image as pilImage
+
 from rest_framework import serializers
-from .models import Post
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Post, Image
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,3 +22,24 @@ class PostUpdateSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         return super().create(validated_data)
+
+class ImageUploadSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    content_base64 = serializers.CharField()
+
+    def create(self, validated_data):
+        content_base64 = validated_data.pop('content_base64')
+        try:
+            img_binary = base64.b64decode(content_base64)
+            image = pilImage.open(BytesIO(img_binary))
+            image.verify()
+            img_obj = Image.objects.create(content=img_binary)        
+            return {
+                "content_base64": content_base64,
+                "id": img_obj.id
+            }
+        except Exception as ex:
+            raise serializers.ValidationError({
+                "error": "The data is invalid.",
+                "description": ex
+            })
